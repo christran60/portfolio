00f215befa10d033f73deb51cc7b1866c84b8f08
Chris <ctran209@outlook.com>
2021-06-14 22:50:34 -0700

    commit

---
+++ my-portfolio/src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Body() {
+    let skills = ["C++", "C", "HTML", "CSS", "Javascript", "React"]
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Body
---
+++ my-portfolio/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import pfp from "../images/sovapfpblue.png"
+import "./Header.css"
+function Header() {
+    return (
+        <div className="Headcontainer">
+            <div className="nameandbiocontainer">
+                <div className="namecont">
+                Christopher Tran
+                </div>
+                <div className="biocont">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, sequi.
+                </div>
+            </div>
+         <div className=" piccontainer  ">
+                <div className="image">
+                    <img className="actualpfp" src={pfp}/>
+                </div>
+            </div> 
+        </div>
+    )
+}
+
+export default Header
