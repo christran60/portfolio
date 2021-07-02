e41998224961d61afe074ce3a22058ae75280ac0
Chris <ctran209@outlook.com>
2021-07-02 13:21:37 -0700

    m

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,8 +1,19 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import Header from './components/Header';
+import Body from './components/Body';
+import Body2 from './components/Body2';
+import Body3 from './components/Body3';
+import ContactMe from './components/ContactMe'
 function App() {
   return (
     <div className="App">
-      
+          <Header />
+    <Body />
+    <Body2 />
+    <Body3 />
+    <ContactMe />
     </div>
   );
 }
---
+++ my-portfolio/src/components/ContactMe.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./ContactMe.css"
+function ContactMe() {
+    return (
+        <div className="ContactMeCont">
+            <div className="supercont">
+                <div className="titlecont">
+                    Get Connected With Me! 
+                </div>
+                <div className="contactmebuttoncont">
+                    <a
+                        className="contactlink"
+                        >
+                        Contact Me
+                    </a>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default ContactMe
