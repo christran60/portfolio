57fc71b628dbfe774ac8b7af7cd6826abe85096b
Chris <ctran209@outlook.com>
2021-07-02 21:39:16 -0700

    m

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-import Header from './components/Header';
-import Body from './components/Body';
-import Body2 from './components/Body2';
-import Body3 from './components/Body3';
-import ContactMe from './components/ContactMe'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Body2 from "./components/Body2";
+import Body3 from "./components/Body3";
+import ContactMe from "./components/ContactMe";
+import ContactPage from "./ContactPage";
+import { Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-          <Header />
-    <Body />
-    <Body2 />
-    <Body3 />
-    <ContactMe />
+      {/* <Route exact path= "/" component = {Header, Body, Body2} />
+       */}
+      {/* <Header />
+     <Body />
+     <Body2 />
+     <Body3 />
+     <ContactMe /> */}
+      {/* <Route exact path="/ContactPage" component={ContactPage} /> */}
     </div>
   );
 }
--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Header from './components/Header';
-import Body from './components/Body';
-import Body2 from './components/Body2';
-import Body3 from './components/Body3';
-import ContactMe from './components/ContactMe'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Body2 from "./components/Body2";
+import Body3 from "./components/Body3";
+import ContactMe from "./components/ContactMe";
+import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Header />
@@ -13,8 +15,9 @@ ReactDOM.render(
     <Body2 />
     <Body3 />
     <ContactMe />
-
   </React.StrictMode>,
-  document.getElementById('root')
+  // <BrowserRouter>
+  //   <App />
+  // </BrowserRouter>,
+  document.getElementById("root")
 );
-
