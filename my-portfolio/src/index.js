3dfe4f26dc7f593e55300515f11c54bc57e7e86b
Chris <ctran209@outlook.com>
2021-07-04 16:52:59 -0700

    m

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+// import NavBar from './NavBar'
 // import Home from "./Home";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <BrowserRouter>
+  {/* <NavBar /> */}
     <App />
   </BrowserRouter>,
   document.getElementById("root")
