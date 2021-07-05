254c2b6cc670bd7efe038dc21a60f4d7d4b3e3b4
Chris <ctran209@outlook.com>
2021-07-04 17:19:29 -0700

    man i do not understand how this nav bar gonna work

--- my-portfolio/src/NavBar.js
+++ my-portfolio/src/NavBar.js
@@ -6,7 +6,7 @@ function NavBar() {
     // make contact me link different from other links so the user knows it goes to a different page
     <div className="NavBarcont">
       <div className="Supercont">
-        <div className="Logo">{/* my logo */}</div>
+        <div className="Logo">my logo</div>
         <div className="Links">
           <Link to="/ContactPage" className="contact">
             Contact Me
