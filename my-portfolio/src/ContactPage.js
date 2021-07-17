35f5ed101dce41e3392f7d215f9484b81b66d7c8
Chris <ctran209@outlook.com>
2021-07-17 15:50:08 -0700

    changed url for fetch

--- my-portfolio/src/ContactPage.js
+++ my-portfolio/src/ContactPage.js
@@ -35,7 +35,7 @@ const ContactPage = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("http://localhost:3000/ContactPage", {
+    let response = await fetch("https://christophertran.netlify.app/ContactPage", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
